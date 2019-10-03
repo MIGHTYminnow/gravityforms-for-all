02e65d7fe08403352a8f4b36bfaccb41487a3ec4
Aaron Barraza <aaron@mightyminnow.com>
2019-10-02 22:50:02 -0700

    [TAG] Version 1.0.0

---
+++ gravityforms-for-all.js
@@ -0,0 +1,50 @@
+jQuery(document).on('gform_post_render', function(){
+	/**
+	 * Implement autocomplete attribute.
+	 */
+	jQuery( '.ginput_container_name .name_first input' ).attr( 'autocomplete', 'given-name' );
+	jQuery( '.ginput_container_name .name_last input' ).attr( 'autocomplete', 'family-name' );
+	jQuery( '.ginput_container_email input' ).attr( 'autocomplete', 'email' );
+	jQuery( '.ginput_container_phone input' ).attr( 'autocomplete', 'tel-national' );
+	jQuery( '.ginput_container_text' ).each(function(){
+		setAutocomplete( 'given-name', jQuery( this ), [
+			'First Name',
+		]);
+		setAutocomplete( 'family-name', jQuery( this ), [
+			'Last Name',
+		]);
+		setAutocomplete( 'organization', jQuery( this ), [
+			'Company',
+			'Organization',
+			'Organization/Company',
+		]);
+	});
+
+	/**
+	 * Make checkboxes fields more accessible:
+	 * - Add role="group" and aria-label to checkbox fields.
+	 * - Convert .gfield_label labels to span on checkboxes to avoid
+	 *   double labels on the same field.
+	 */
+	jQuery( '.ginput_container_checkbox' ).each(function(){
+		var $label = jQuery( this ).closest( '.gfield' ).find( '.gfield_label' );
+		var html = $label.html();
+		var label = $label.text();
+		$label.after( '<span class="gfield_label">' + html + '<span>' );
+		$label.remove();
+		jQuery( this ).attr( 'role', 'group' ).attr( 'aria-label', label );
+	});
+
+	/**
+	 * Set focus on the first input with errors.
+	 */
+	jQuery( '.gfield_error input' ).eq(0).focus();
+
+	/**
+	 * Implement aria-describedby to error inputs.
+	 */
+	jQuery( '.gfield_error input, .gfield_error select, .gfield_error textarea' ).each(function(){
+		var id = jQuery(this).closest( '.gfield' ).find( '.validation_message' ).attr( 'id' );
+		jQuery(this).attr( 'aria-describedby', '#' + id );
+	});
+});
---
+++ gravityforms-for-all.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Gravity Forms for All
+ * Plugin URI: https://github.com/MIGHTYminnow/gravityforms-for-all
+ * Description: Makes your Gravity Forms accessible.
+ * Version: 1.0.0
+ * Author: MIGHTYminnow
+ * Author URI: https://mightyminnow.com
+ */
+
+/**
+ * Enqueue child theme styles and scripts.
+ */
+if ( ! function_exists( 'gfa_enqueue_assets' ) ) {
+	add_action( 'wp_enqueue_scripts', 'gfa_enqueue_assets' );
+	function gfa_enqueue_assets() {
+		wp_enqueue_script( 'gravityforms-for-all', plugin_dir_url( __FILE__ ) . 'gravityforms-for-all.js', array( 'jquery' ), '1.0.0', true );
+	}
+}
